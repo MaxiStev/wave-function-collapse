74526974c05290de64e04e82e031a57b318f3844
Max Stever <max.stever@tecbeast.gmbh>
2022-07-13 14:34:39 +0200

    some types

--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,44 @@
 //╚
 
 
-
-struct CellOp {
+type CellOp = Option<CellContent>;
+struct CellContent {
     top: u8,
     right: u8,
     bottom: u8,
     left: u8,
     content: char
 }
-impl Default for CellOp {
+impl Default for CellContent {
     fn default() -> Self {
-        CellOp {top: 0, right: 0, bottom: 0, left: 0, content: ' '}
+        CellContent {top: 0, right: 0, bottom: 0, left: 0, content: ' '}
     }
 }
 fn main() {
     println!("Hello, world!");
-    let trb = CellOp {content: '╠', top: 2, right: 2, bottom: 2, ..Default::default()};
-    let trl = CellOp {content: '╩', top: 2, right: 2, left: 2, ..Default::default()};
-    let trl = CellOp {content: '╣', top: 2, bottom: 2, left: 2, ..Default::default()};
-    let rbl = CellOp {content: '╦', right: 2, bottom: 2, left: 2, ..Default::default()};
+    let trb = CellContent {content: '╠', top: 2, right: 2, bottom: 2, ..Default::default()};
+    let trl = CellContent {content: '╩', top: 2, right: 2, left: 2, ..Default::default()};
+    let trl = CellContent {content: '╣', top: 2, bottom: 2, left: 2, ..Default::default()};
+    let rbl = CellContent {content: '╦', right: 2, bottom: 2, left: 2, ..Default::default()};
 
     let mut cell1 = Cell::default();
     let mut cell2 = Cell::default();
-    cell1.right = Some(&cell2);
-    cell2.left = Some(&cell1);
 }
-struct Cell<'a> {
-    top: Option<&'a Cell<'a>>,
-    right: Option<&'a Cell<'a>>,
-    bottom: Option<&'a Cell<'a>>,
-    left: Option<&'a Cell<'a>>,
-    content: Option<CellOp>
+struct Cell {
+    content: CellOp
 }
-impl Default for Cell<'_> {
-    fn default() -> Self {
-        Cell {top: None, right: None, bottom: None, left: None, content: None}
+impl Cell {
+    fn possible(allowed: Vec<CellContent>, top: CellOp, right: CellOp, bottom: CellOp, left: CellOp) -> Vec<CellContent> {
+
     }
+}
+impl Default for Cell {
+    fn default() -> Self {
+        Cell {content: None}
+    }
+}
+
+struct Field {
+    field: Vec<Vec<Cell>>,
+    allowed_cells: Vec<CellContent>
 }
